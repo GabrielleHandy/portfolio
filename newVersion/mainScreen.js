b4df8c418e8ded3872e0eec87a136c2f7e9e6022
GabrielleHandy <gehandy123@gmail.com>
2023-10-09 10:15:46 -0400

    Added real time and battery level reaadings and aded more styling

--- newVersion/mainScreen.js
+++ newVersion/mainScreen.js
@@ -3,7 +3,18 @@
 const topTransition = document.getElementById("transitionTop")
 const bottomTransition=document.getElementById("transitionBottom")
 const flowerLoad = document.getElementById("flowerLoad")
-window.onload(transition())
+const time = document.getElementById("time")
+const about = document.getElementById("aboutMe")
+const projects = document.getElementById("projectCard")
+const contacts = document.getElementById("contacts")
+
+const batteryLevelImgs = {8: "images/battery/80-100.svg",
+5:"images/battery/50-80.svg",
+0:"images/battery/0-40.svg"
+}
+const batteryPic = document.getElementById("batteryImg")
+const batteryPercentage = document.getElementById("batteryPercentage")
+transition()
 
 function transition() {
     flowerLoad.classList.add("show")
@@ -11,8 +22,60 @@ function transition() {
     topTransition.classList.add("topTransition")
     bottomTransition.classList.add("bottomTransition")
     
-
+    setTimeout(()=>{flowerLoad.style.zIndex=-1}, 3000)
     
     
 }
-flowerLoad.style.zIndex=-1
+
+setInterval(setTime, 30000)
+function setTime(){
+    const d = new Date();
+let text = d.toLocaleTimeString('en-us',{hour: "2-digit", minute: "2-digit"});
+
+time.innerText =text
+
+}
+setTime()
+
+function updateBattery(battery){
+    
+    battery.charging? batteryPercentage.innerText = `${Math.trunc(battery.level * 100)}%`: battery.level<1?batteryPercentage.innerText = `${Math.trunc(battery.level * 100)}%`: batteryPercentage.innerText = ``;
+    if(battery.charging){
+        batteryPic.src = "images/battery/charging.png"
+    }
+    else{
+        
+        battery.level >= .8? batteryPic.src = batteryLevelImgs[8]: 
+        (battery.level >= .5)?batteryPic.src = batteryLevelImgs[5]:
+        battery.level <= .49?batteryPic.src = batteryLevelImgs[0]:
+        batteryPic.src = batteryLevelImgs[8]
+                
+        
+    }
+}
+navigator.getBattery().then(function(battery) {
+    updateBattery(battery)
+    battery.onlevelchange = function() {
+        updateBattery(battery)
+        
+    };
+    battery.onchargingchange = function () {
+        updateBattery(battery)
+      };
+  });
+
+  function open(div){
+    switch (div) {
+        case "about":
+            about.classList.remove("closed")
+            about.classList.add("open")
+            about.querySelector(".exit").addEventListener("click", ()=>{
+
+            })
+            break;
+    
+        default:
+            break;
+    }
+
+  }
