a0c961fd381c355391d876a88b365b3458e4b9fe
GabrielleHandy <91081738+GabrielleHandy@users.noreply.github.com>
2023-10-14 17:46:41 +0000

    Created dynamic resume experience

--- temp-portfolio/js/scripts.js
+++ temp-portfolio/js/scripts.js
@@ -59,52 +59,89 @@ class TxtType {
 
 window.onload = function() {
     var elements = document.querySelector(".typewrite");
+    if(elements){
+
+        var toRotate = elements.getAttribute('data-type');
+            var period = elements.getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements, JSON.parse(toRotate), period);
+                }
+            
+            // INJECT CSS
+            var css = document.createElement("style");
+            css.type = "text/css";
+            css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+            document.body.appendChild(css);
+
+
+
+
+    }
     
     
-    var toRotate = elements.getAttribute('data-type');
-    var period = elements.getAttribute('data-period');
-    if (toRotate) {
-          new TxtType(elements, JSON.parse(toRotate), period);
-        }
-    
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
 };
 
-// window.onload = function populateExperience() {
-//     const experienceDiv = document.getElementById('experience')
-//     let file = File("/assets/Experience.csv")
-//     let reader = (file)
-//     reader
-// }
-import { createReadStream } from 'fs';
+ 
+    const experienceDiv = document.getElementById('experience')
+    if(experienceDiv){
+       const jobHistory = [
+        {
+          "Job Title": "Information Specialist",
+          "Date": "February 2023 to Present",
+          "Company": "Forsyth County Public Library",
+          "Location": "Winston-Salem, NC",
+          "Job Summary": "Design and create eye-catching flyers and displays to promote library programs and events, leveraging graphic design skills. Efficiently manage library resources and circulation using automated software systems. Assist library patrons in resolving technology-related inquiries, demonstrating strong technical aptitude. Responsible for maintaining and organizing digital records and data using Microsoft Excel. Utilize library software to streamline processes such as issuing library cards, reserving rooms, and managing book holds."
+        },
+        {
+          "Job Title": "Developer Internship at Shopify",
+          "Date": "May 2022- June 2022 (Left early for personal reasons)",
+          "Company": "Shopify Canada",
+          "Location": "Canada",
+          "Job Summary": "Hands-on experience with writing and pushing code that improved the backend code base of Shopify’s product with guided mentorship. Followed Agile methodology, and participated in daily scrum meetings, project planning, and sprints involving multiple areas of development. Experience with Ruby."
+        },
+        {
+          "Job Title": "Software Engineering Fellowship",
+          "Date": "November 2021- January 2022",
+          "Company": "Hackbright Academy",
+          "Location": "San Francisco, CA",
+          "Job Summary": "Exponentially grew knowledge and coding experience using hands-on practice, projects, and guided mentorship. Part of an empowered team working on delivering quality software projects in an agile environment. Examples of projects include: Movie ratings app: Tech used: HTML, Jinja2, Flask, SQLAlchemy. Shark-themed hangman game: Tech used: Ajax, Javascript, Flask. Melon shopping site -Backend- Tech used: Flask, Jinja. Followed Agile methodology, and participated in daily scrum meetings, project planning, and sprints."
+        },
+        {
+          "Job Title": "Manga Translator with Tech and Translation Skills",
+          "Date": "March 2020 to May 2020",
+          "Company": "JianLai Global",
+          "Location": "Remote",
+          "Job Summary": "Skillfully translated Chinese manga content for a Chinese manga website, employing bilingual proficiency. Edited and enhanced previously translated work for accuracy and clarity. Developed and maintained a comprehensive glossary to streamline future translation projects."
+        }
+      ]
 
-createReadStream('/assets/Experience.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    // Each row is an object with columns as keys
-    console.log(row);
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed.');
-  });
 
-
-var experienceHtml = `<div class="card shadow border-0 rounded-4 mb-5">
+          for(let job in jobHistory){
+            console.log(job)
+            let experience = document.createElement('div')
+            experience.innerHTML= `<div class="card shadow border-0 rounded-4 mb-5">
                                 <div class="card-body p-5">
                                     <div class="row align-items-center gx-5">
                                         <div class="col text-center text-lg-start mb-4 mb-lg-0">
                                             <div class="bg-light p-4 rounded-4">
-                                                <div class="text-primary fw-bolder mb-2">${jobDate}</div>
-                                                <div class="small fw-bolder">${jobTitle}</div>
-                                                <div class="small text-muted">${company}</div>
-                                                <div class="small text-muted">${jobLocation}</div>
+                                                <div class="text-primary fw-bolder mb-2">${jobHistory[job]["Date"]}</div>
+                                                <div class="small fw-bolder">${jobHistory[job]["Job Title"]}</div>
+                                                <div class="small text-muted">${jobHistory[job]["Company"]}</div>
+                                                <div class="small text-muted">${jobHistory[job]["Location"]}</div>
                                             </div>
                                         </div>
-                                        <div class="col-lg-8"><div>${jobSummary}</div></div>
+                                        <div class="col-lg-8"><div>${jobHistory[job]["Job Summary"]}</div></div>
                                     </div>
                                 </div>
                             </div>`
+
+
+            experienceDiv.appendChild(experience)
+
+          }
+
+    }
+    
+
+
+
